b45d0cc07754bc0f5099422bb5760a94f3919cc2
Pretham Muthappa <prethambachira1819@gmail.com>
2023-09-17 18:25:26 +0530

    new files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const app=express();
+const path=require('path')
+dotenv.config();
+app.use(express.static(path.join(__dirname,"src")))
+app.use(express.static(path.join(__dirname,"dist")))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs");
+
+const port=process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(port,()=>{
+    console.log("list",port);
+})
